7dfd507c842775f70f23e53af6e39ab6c17d06dc
zanevska <47556230+zanevska@users.noreply.github.com>
2019-04-09 11:59:56 +0300

    Add files via upload

---
+++ задание 5/MainScript2.js
@@ -0,0 +1,143 @@
+var arrOfPosts = [
+    {
+        id: '1',
+        description: 'Добрый день, что может послужить отказом медицинскому работнику в приеме на работу?',
+        author: 'jstcandid',
+        ImgLink: 'https://i1.rozetka.ua/goods/10529970/69985802_images_10529970560.jpg'
+    },
+    {
+        id: '2',
+        description: 'Здравствуйте. Что понимается под врачебной тайной?',
+        author: 'unknown1',
+        ImgLink: 'https://toptos.com.ua/images/16/modnyy-derzhatel-dlya-telefona-popkholder-s-edinorogom-v-ochkakh-serdtse-75898559123532_small6.jpg'
+    },
+    {
+        id: '3',
+
+        description: 'Добрый день. Сколько существует этапов проведения конкурса на замещение вакантной должности государственной гражданской службы и  что они из себя представляют?',
+        author: 'lawyer',
+        ImgLink: 'https://toptos.com.ua/images/26/popsoket-popholder-im-unicorn-prikolnyy-58922587287537_small6.jpg'
+    },
+    {
+        id: '4',
+
+        description: 'Добрый день, как проводится конкурс на замещение вакантной должности государственной гражданской службы?',
+        author: 'bestlawyer',
+        ImgLink: 'https://zakazposterov.ru/fotooboi/z/fotooboi-e-47425-vektornie-illyustratsii-s-miliy-muljtfiljm-edinorog-eli-vkusni-zakazposterov-ru_z.jpg'
+    },
+    {
+        id: '5',
+
+        description: 'Доброе утро. Какие есть группы должностей государственной гражданской службы?',
+        author: 'bestlawyerever',
+        ImgLink: 'https://картинки-для-срисовки.рф/image/cache/catalog/vse-kartinki/skvishi/skvishi-68-800x800.jpg'
+    },
+    {
+        id: '6',
+
+        description: 'Здравствуйте. В каком случае возникает материальная ответственность у водителя?',
+        author: 'Ivan',
+        ImgLink: 'https://png.pngtree.com/element_our/png_detail/20180926/cute-cat-unicorn-illustration-png_114026.jpg'
+    },
+    {
+        id: '7',
+
+        description: 'День добрый! Как определить по записям в трудовой книжке непрерывный трудовой стажв целях исчисления пособия по временной нетрудоспособности?',
+        author: 'Mimimchik',
+        ImgLink: 'https://thumbs.dreamstime.com/z/иллюстрация-нарисованная-рукой-волшебного-единорога-изолированная-113128211.jpg'
+    },
+    {
+        id: '8',
+
+        description: 'Здравствуйте. Какая будет ответственность за нарушение правил ведения и хранения трудовых книжек?',
+        author: 'Snusmumr',
+        ImgLink: 'https://www.bookvoed.ru/files/1836/32/43/32/9.jpg'
+    },
+    {
+        id: '9',
+
+        description: 'Как оформляется приказ об отстранении при приостановлении специального права на срок до двух месяцев, если работник отказался от перевода на другую работу?',
+        author: 'Ivanov',
+        ImgLink: 'http://ae01.alicdn.com/kf/HTB107EpasfrK1RkSnb4q6xHRFXaX/Cute-Animal-Unicorn-Light-Switch-Stickers-For-Kids-Rooms-Baby-Nursery-Home-Cartoon-Horse-Unicorn-Cup.jpg_220x220q90.jpg'
+    },
+    {
+        id: '10',
+
+        description: 'Интересует такой вопрос, могу ли я подать заявление на расторжение брака не имея прописки?',
+        author: 'Whale',
+        ImgLink: 'https://s.tcdn.co/517/ec0/517ec0c4-203a-39e1-a6dc-a7801bfd6d6b/2.png'
+    },
+    {
+        id: '11',
+
+        description: 'вопрос 11',
+        author: 'Иванов Иван',
+        ImgLink: 'https://toptos.com.ua/images/17/krasivyy-derzhatel-popsoket-dlya-devushki-edinorog-19350168191724_small6.jpg'
+    },
+    {
+        id: '12',
+
+        description: 'вопрос 12',
+        author: 'Иванов Иван',
+        ImgLink: 'https://toptos.com.ua/images/17/krasivyy-derzhatel-popsoket-dlya-devushki-edinorog-19350168191724_small6.jpg'
+    },
+    {
+        id: '13',
+
+        description: 'вопрос 13',
+        author: 'Иванов Иван',
+        ImgLink: 'https://toptos.com.ua/images/17/krasivyy-derzhatel-popsoket-dlya-devushki-edinorog-19350168191724_small6.jpg'
+    },
+    {
+        id: '14',
+
+        description: 'вопрос 14',
+        author: 'Иванов Иван',
+        ImgLink: 'https://toptos.com.ua/images/17/krasivyy-derzhatel-popsoket-dlya-devushki-edinorog-19350168191724_small6.jpg'
+    },
+    {
+        id: '15',
+
+        description: 'вопрос 15',
+        author: 'Иванов Иван',
+        ImgLink: 'https://toptos.com.ua/images/17/krasivyy-derzhatel-popsoket-dlya-devushki-edinorog-19350168191724_small6.jpg'
+    },
+    {
+        id: '16',
+
+        description: 'вопрос 16',
+        author: 'Иванов Иван',
+        ImgLink: 'https://toptos.com.ua/images/17/krasivyy-derzhatel-popsoket-dlya-devushki-edinorog-19350168191724_small6.jpg'
+    },
+    {
+        id: '17',
+
+        description: 'вопрос 17',
+        author: 'Иванов Иван',
+        ImgLink: 'https://toptos.com.ua/images/17/krasivyy-derzhatel-popsoket-dlya-devushki-edinorog-19350168191724_small6.jpg'
+    },
+    {
+        id: '18',
+
+        description: 'вопрос 18',
+        author: 'Иванов Иван',
+        ImgLink: 'https://toptos.com.ua/images/17/krasivyy-derzhatel-popsoket-dlya-devushki-edinorog-19350168191724_small6.jpg'
+    },{
+        id: '19',
+
+        description: 'вопрос 19',
+        author: 'Иванов Иван',
+        ImgLink: 'https://toptos.com.ua/images/17/krasivyy-derzhatel-popsoket-dlya-devushki-edinorog-19350168191724_small6.jpg'
+    },
+    {
+        id: '20',
+
+        description: 'вопрос 20',
+        author: 'Иванов Иван',
+        ImgLink: 'https://toptos.com.ua/images/17/krasivyy-derzhatel-popsoket-dlya-devushki-edinorog-19350168191724_small6.jpg'
+    }
+
+
+];
+
+
